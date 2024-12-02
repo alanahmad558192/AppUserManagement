eda8431379246719775a8b3749e52e7f70d14710
Alan Ahmad <alanahmad558192@outlook.com>
2024-12-02 13:05:36 +0800

    Initial commit: User Management Application setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserManagementComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UserManagementComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }
+];
